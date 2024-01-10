2ddb7a03dc5e181ceab5b6b70d4eb1ed3f6129b9
calebmutisya <mutisyacalebmusau@gmail.com>
2024-01-10 13:53:38 +0300

    Complete and Working

--- server/app.py
+++ server/app.py
@@ -80,3 +80,7 @@ def enclosure_by_id(id):
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+# <li>Species: {animal.species}</li>
+# <li>Zookeeper: {animal.zookeeper_id.name}</li>
+# <li>Enclosure: {animal.enclosure_id.environment}</li>
